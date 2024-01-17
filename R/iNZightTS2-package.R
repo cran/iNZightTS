6f1d61978a73a7af5d75ad8a04d8af32ae1de522
Tom Elliott <tom.elliott@auckland.ac.nz>
2024-01-17 06:20:02 +0000

    version 2.0.0

--- R/iNZightTS2-package.R
+++ R/iNZightTS2-package.R
@@ -1,7 +1,6 @@
 #' The iNZightTS package provides some simple analysis tools for
 #' exploring time series data. It is used in the iNZight software.
 #'
-#'
 #' @title Time Series Data Analysis
 #'
 #' @author Tom Elliott (previously: Marco Kuper, Simon Potter, and David Banks)
@@ -10,11 +9,18 @@
 #'
 #' @keywords iNZight
 #'
-#' @seealso \code{iNZight}
+#' @seealso \code{\link[iNZightTS]{inzightts}}
 #'
 #' @import grid grDevices graphics stats utils
-#' @importFrom magrittr '%>%'
 #' @importFrom rlang .data
+#' @importFrom rlang ':='
 #'
 #' @name iNZightTS-package
 NULL
+
+
+utils::globalVariables(c(
+    "trend", "remainder", "seasonal", "residual", "Date", "value", ".key", ".x",
+    "Prediction", "index", ".var", ".model", ".mean", ".lower", ".upper", ".y",
+    ".fitted", ".", "P", "Q", "constant", "d", "p", ".rows", "id", ".yint", "z"
+))
